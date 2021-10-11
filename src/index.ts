269e6467210a52a63c47b31732ad00e11785c877
Kevin Malakoff <kmalakoff@gmail.com>
2021-10-10 23:04:16 -0700

    Added support for axios, got, undici

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { AxiosResponse } from "axios";
+import { Response as GotResponse } from "got";
 import { Response as NodeResponse } from "node-fetch";
+import { Readable as NodeReadableStream } from "stream";
+import { Response as UndiciResponse } from "undici";
 
 interface CrossFetchResponse {
   _bodyBlob: Blob;
@@ -49,24 +53,38 @@ function promiseIterator<T>(promise): AsyncIterableIterator<T> {
 }
 /* c8 ignore stop */
 
+export type ResponseType = AxiosResponse | CrossFetchResponse | GotResponse | NodeResponse | Response | UndiciResponse;
+
 /**
  * @param response A response. Supports fetch, node-fetch, and cross-fetch
  */
-export default function responseIterator<T>(
-  response: Response | NodeResponse | CrossFetchResponse
-): AsyncIterableIterator<T> {
+export default function responseIterator<T>(response: ResponseType): AsyncIterableIterator<T> {
   if (response === undefined) throw new Error("Missing response for responseIterator");
 
-  // node-fetch
+  // node node-fetch
   if ((response as NodeResponse).body && (response as NodeResponse).body[Symbol.asyncIterator] !== undefined)
     return streamIterator<T>((response as NodeResponse).body);
   /* c8 ignore start */
-  // browser fetch
+  // browser fetch or undici
   else if ((response as Response).body && (response as Response).body.getReader)
     return readerIterator<T>((response as Response).body.getReader());
+  // cross platform axios
+  else if ((response as AxiosResponse).data) {
+    if ((response as AxiosResponse<Blob>).data.stream)
+      return readerIterator<T>(
+        ((response as AxiosResponse<Blob>).data.stream() as unknown as ReadableStream<Uint8Array>).getReader()
+      );
+    else if ((response as AxiosResponse<NodeReadableStream>).data[Symbol.asyncIterator] !== undefined)
+      return streamIterator<T>((response as AxiosResponse<NodeReadableStream>).data);
+  }
+
   // browser cross-fetch
   else if ((response as CrossFetchResponse)._bodyBlob)
     return promiseIterator<T>((response as CrossFetchResponse)._bodyBlob.arrayBuffer());
+  // node got
+  else if ((response as GotResponse).readable && (response as GotResponse)[Symbol.asyncIterator] !== undefined)
+    return streamIterator<T>(response as GotResponse);
+
   /* c8 ignore stop */
 
   throw new Error("Unknown body type for responseIterator");
--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,5 +1,6 @@
 import { assert } from "chai";
 import "isomorphic-fetch";
+import axios from "axios";
 import crossFetch from "cross-fetch";
 import responseIterator from "response-iterator";
 import decodeUTF8 from "../lib/decodeUTF8.cjs";
@@ -26,6 +27,19 @@ describe("response-iterator", function () {
 
   it("cross-fetch", async function () {
     const res = await crossFetch("https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json");
+
+    let data = "";
+    for await (const chunk of responseIterator(res)) {
+      data += decodeUTF8(chunk);
+    }
+    assert.deepEqual(JSON.parse(data).name, "response-iterator");
+  });
+
+  it("axios stream", async function () {
+    const res = await axios({
+      url: "https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json",
+      responseType: typeof window === "undefined" ? "stream" : "blob",
+    });
 
     let data = "";
     for await (const chunk of responseIterator(res)) {
