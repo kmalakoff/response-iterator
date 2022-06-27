7ab16a811e8adadd172c404c37de07d00764738e
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-27 00:14:32 -0700

    Update ts-dev-stack

--- test/lib/polyfill.cjs
+++ test/lib/polyfill.cjs
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 function polyfill() {
-  if (typeof Promise === "undefined") {
+  if (typeof Promise === 'undefined') {
     /* eslint-disable-next-line no-undef */
-    const root = typeof window === "undefined" ? global : window;
-    root.Promise = require("pinkie-promise");
+    const root = typeof window === 'undefined' ? global : window;
+    root.Promise = require('pinkie-promise');
   }
 
-  if (typeof Buffer !== "undefined" && !Buffer.from) {
+  if (typeof Buffer !== 'undefined' && !Buffer.from) {
     Buffer.from = function from(data, encoding) {
       return new Buffer(data, encoding);
     };
