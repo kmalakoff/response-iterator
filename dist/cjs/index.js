7e70b5e41fac4ec31401102aee20460f8ec90013
Kevin Malakoff <kmalakoff@gmail.com>
2022-08-10 21:18:46 -0700

    Upgrade modules

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -2,11 +2,24 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-module.exports = responseIterator;
-var _asyncTs = _interopRequireDefault(require("./iterators/async.js"));
-var _nodeStreamTs = _interopRequireDefault(require("./iterators/nodeStream.js"));
-var _promiseTs = _interopRequireDefault(require("./iterators/promise.js"));
-var _readerTs = _interopRequireDefault(require("./iterators/reader.js"));
+Object.defineProperty(exports, /**
+ * @param response A response. Supports fetch, node-fetch, and cross-fetch
+ */ "default", {
+    enumerable: true,
+    get: function() {
+        return responseIterator;
+    }
+});
+var _asyncTs = /*#__PURE__*/ _interopRequireDefault(require("./iterators/async.js"));
+var _nodeStreamTs = /*#__PURE__*/ _interopRequireDefault(require("./iterators/nodeStream.js"));
+var _promiseTs = /*#__PURE__*/ _interopRequireDefault(require("./iterators/promise.js"));
+var _readerTs = /*#__PURE__*/ _interopRequireDefault(require("./iterators/reader.js"));
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+var hasIterator = typeof Symbol !== "undefined" && Symbol.asyncIterator;
 function responseIterator(response) {
     if (response === undefined) throw new Error("Missing response for responseIterator");
     // determine the body
@@ -15,16 +28,16 @@ function responseIterator(response) {
     else if (response.data) body = response.data;
     else if (response._bodyBlob) body = response._bodyBlob; // cross-fetch
     /* c8 ignore stop */ // adapt the body
-    if (hasIterator && body[Symbol.asyncIterator]) return (0, _asyncTs).default(body);
-    /* c8 ignore start */ if (body.getReader) return (0, _readerTs).default(body.getReader());
-    if (body.stream) return (0, _readerTs).default(body.stream().getReader());
-    if (body.arrayBuffer) return (0, _promiseTs).default(body.arrayBuffer());
-    if (body.pipe) return (0, _nodeStreamTs).default(body);
+    if (hasIterator && body[Symbol.asyncIterator]) return (0, _asyncTs.default)(body);
+    /* c8 ignore start */ if (body.getReader) return (0, _readerTs.default)(body.getReader());
+    if (body.stream) return (0, _readerTs.default)(body.stream().getReader());
+    if (body.arrayBuffer) return (0, _promiseTs.default)(body.arrayBuffer());
+    if (body.pipe) return (0, _nodeStreamTs.default)(body);
     /* c8 ignore stop */ throw new Error("Unknown body type for responseIterator. Maybe you are not passing a streamable response");
 }
-function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : {
-        default: obj
-    };
+
+if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {
+  Object.defineProperty(exports.default, '__esModule', { value: true });
+  for (var key in exports) exports.default[key] = exports[key];
+  module.exports = exports.default;
 }
-var hasIterator = typeof Symbol !== "undefined" && Symbol.asyncIterator;
