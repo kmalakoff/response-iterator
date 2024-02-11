3b34c7a9cd0cac7899648117c34d3b6a9f471745
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-11 03:22:46 -0800

    Upgrade dependencies

--- test/unit/index.test-node.ts
+++ test/unit/index.test-node.ts
@@ -1,19 +1,19 @@
-import '../lib/polyfill.cjs';
 import assert from 'assert';
 import got from 'got';
 import nodeFetch from 'node-fetch';
-import { fetch as undiciFetch } from 'undici';
 import responseIterator from 'response-iterator';
+import { fetch as undiciFetch } from 'undici';
 import decodeUTF8 from '../lib/decodeUTF8.cjs';
+import '../lib/polyfill.cjs';
 import toText from '../lib/toText.cjs';
 
 const hasAsync = typeof process !== 'undefined' && +process.versions.node.split('.')[0] > 10;
 
-describe('response-iterator node', function () {
-  it('node-fetch', function (done) {
-    nodeFetch('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json').then(function (res) {
+describe('response-iterator node', () => {
+  it('node-fetch', (done) => {
+    nodeFetch('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json').then((res) => {
       try {
-        toText(responseIterator(res)).then(function (data) {
+        toText(responseIterator(res)).then((data) => {
           assert.deepEqual(JSON.parse(data).name, 'response-iterator');
           done();
         });
@@ -24,7 +24,7 @@ describe('response-iterator node', function () {
   });
 
   !hasAsync ||
-    it('node-fetch - async', async function () {
+    it('node-fetch - async', async () => {
       const res = await nodeFetch('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json');
 
       let data = '';
@@ -34,10 +34,10 @@ describe('response-iterator node', function () {
       assert.deepEqual(JSON.parse(data).name, 'response-iterator');
     });
 
-  it('got stream', function (done) {
+  it('got stream', (done) => {
     const res = got.stream('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json');
     try {
-      toText(responseIterator(res)).then(function (data) {
+      toText(responseIterator(res)).then((data) => {
         assert.deepEqual(JSON.parse(data).name, 'response-iterator');
         done();
       });
@@ -47,10 +47,10 @@ describe('response-iterator node', function () {
   });
 
   !undiciFetch ||
-    it('undici', function (done) {
-      undiciFetch('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json').then(function (res) {
+    it('undici', (done) => {
+      undiciFetch('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json').then((res) => {
         try {
-          toText(responseIterator(res)).then(function (data) {
+          toText(responseIterator(res)).then((data) => {
             assert.deepEqual(JSON.parse(data).name, 'response-iterator');
             done();
           });
