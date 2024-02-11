2765799fe6e8bbf9309e2c1c3332d69b29000bf0
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-10 22:07:51 -0800

    Upgrade modules

--- test/lib/toText.cjs
+++ test/lib/toText.cjs
@@ -1,12 +1,11 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const decodeUTF8 = require('./decodeUTF8.cjs');
 
 function text(data, iterator) {
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     iterator
       .next()
-      .then(function (next) {
-        if (next.value !== undefined) data += decodeUTF8(next.value);
+      .then((next) => {
+        data += decodeUTF8(next.value);
         if (next.done) return resolve(data);
         text(data, iterator).then(resolve).catch(reject);
       })
