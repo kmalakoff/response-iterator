2d368aedc6ee04c234c0b6eee0aebcfa71142742
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-10 22:07:51 -0800

    Upgrade modules

--- test/lib/polyfill.cjs
+++ test/lib/polyfill.cjs
@@ -1,8 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 function polyfill() {
   if (typeof Promise === 'undefined') {
-    /* eslint-disable-next-line no-undef */
     const root = typeof window === 'undefined' ? global : window;
     root.Promise = require('pinkie-promise');
   }
