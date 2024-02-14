8f5ac9c9143c41493a84ae545c2dace0be33cc6c
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-14 08:26:13 -0800

    Update to ts-dev-stack

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,7 +1,7 @@
-import '../lib/polyfill.cjs';
 import assert from 'assert';
 import responseIterator from 'response-iterator';
 import decodeUTF8 from '../lib/decodeUTF8.cjs';
+import '../lib/polyfill.cjs';
 import stringStream from '../lib/stringStream.cjs';
 import toText from '../lib/toText.cjs';
 
@@ -9,8 +9,8 @@ const hasIterator = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
 const hasAsync = typeof process !== 'undefined' && +process.versions.node.split('.')[0] > 10;
 const hasConst = typeof process !== 'undefined' && +process.versions.node.split('.')[0] > 0;
 
-describe('response-iterator', function () {
-  it('error: no response', function () {
+describe('response-iterator', () => {
+  it('error: no response', () => {
     try {
       responseIterator(undefined);
       assert.ok(false);
@@ -19,7 +19,7 @@ describe('response-iterator', function () {
     }
   });
 
-  it('error: unexpected response', function () {
+  it('error: unexpected response', () => {
     const response = 'not-a-response';
     try {
       responseIterator(response as unknown as Response);
@@ -29,20 +29,18 @@ describe('response-iterator', function () {
     }
   });
 
-  it('string stream', function (done) {
+  it('string stream', (done) => {
     const res = stringStream('{ "name": "response-iterator"}');
-    try {
-      toText(responseIterator(res)).then(function (data) {
+    toText(responseIterator(res))
+      .then((data) => {
         assert.deepEqual(JSON.parse(data).name, 'response-iterator');
         done();
-      });
-    } catch (err) {
-      done(err);
-    }
+      })
+      .catch((err) => done(err));
   });
 
   !hasAsync ||
-    it('string stream - async', async function () {
+    it('string stream - async', async () => {
       const res = stringStream('{ "name": "response-iterator"}');
 
       const iter = responseIterator(res);
@@ -55,41 +53,37 @@ describe('response-iterator', function () {
     });
 
   !hasConst ||
-    it('axios stream or blob', function (done) {
+    it('axios stream or blob', (done) => {
       import('axios')
-        .then(function (axios) {
+        .then((axios) => {
           axios
             .default({
               url: 'https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json',
               responseType: typeof window === 'undefined' ? 'stream' : 'blob',
             })
-            .then(function (res) {
-              try {
-                toText(responseIterator(res)).then(function (data) {
+            .then((res) => {
+              toText(responseIterator(res))
+                .then((data) => {
                   assert.deepEqual(JSON.parse(data).name, 'response-iterator');
                   done();
-                });
-              } catch (err) {
-                done(err);
-              }
+                })
+                .catch((err) => done(err));
             });
         })
         .catch(done);
     });
 
   !hasConst ||
-    it('fetch or node-fetch', function (done) {
+    it('fetch or node-fetch', (done) => {
       import('isomorphic-fetch')
-        .then(function () {
-          fetch('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json').then(function (res) {
-            try {
-              toText(responseIterator(res)).then(function (data) {
+        .then(() => {
+          fetch('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json').then((res) => {
+            toText(responseIterator(res))
+              .then((data) => {
                 assert.deepEqual(JSON.parse(data).name, 'response-iterator');
                 done();
-              });
-            } catch (err) {
-              done(err);
-            }
+              })
+              .catch((err) => done(err));
           });
         })
         .catch(done);
@@ -98,7 +92,7 @@ describe('response-iterator', function () {
   !hasIterator ||
     !hasAsync ||
     typeof fetch === 'undefined' ||
-    it('fetch or node-fetch - async', async function () {
+    it('fetch or node-fetch - async', async () => {
       const res = await fetch('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json');
 
       let data = '';
@@ -109,18 +103,16 @@ describe('response-iterator', function () {
     });
 
   !hasConst ||
-    it('cross-fetch', function (done) {
+    it('cross-fetch', (done) => {
       import('cross-fetch')
-        .then(function (crossFetch) {
-          crossFetch.default('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json').then(function (res) {
-            try {
-              toText(responseIterator(res)).then(function (data) {
+        .then((crossFetch) => {
+          crossFetch.default('https://raw.githubusercontent.com/kmalakoff/response-iterator/master/package.json').then((res) => {
+            toText(responseIterator(res))
+              .then((data) => {
                 assert.deepEqual(JSON.parse(data).name, 'response-iterator');
                 done();
-              });
-            } catch (err) {
-              done(err);
-            }
+              })
+              .catch((err) => done(err));
           });
         })
         .catch(done);
