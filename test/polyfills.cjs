7b0fd41b84c64500bd6018145feea310aa7497e5
Kevin Malakoff <kmalakoff@gmail.com>
2024-11-29 06:49:41 -0800

    Upgrade modules

---
+++ test/polyfills.cjs
@@ -0,0 +1,14 @@
+var root = typeof window === 'undefined' ? global : window;
+if (typeof Promise === 'undefined') {
+  root.Promise = require('pinkie-promise');
+}
+
+if (typeof setImmediate === 'undefined') {
+  root.setImmediate = require('next-tick');
+}
+
+if (typeof Buffer !== 'undefined' && !Buffer.from) {
+  Buffer.from = function from(data, encoding) {
+    return new Buffer(data, encoding);
+  };
+}
