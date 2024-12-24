c3504ae9d3d09b71d51f39a5ee6cc91d892be6b2
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 09:41:37 -0800

    Upgrade modules

--- test/exports/import.test.mjs
+++ test/exports/import.test.mjs
@@ -1,19 +1,8 @@
-import '../lib/polyfill.cjs';
 import assert from 'assert';
 import responseIterator from 'response-iterator';
-import stringStream from '../lib/stringStream.cjs';
-import toText from '../lib/toText.cjs';
 
-describe('exports .mjs', function () {
-  it('string stream', function (done) {
-    const res = stringStream('{ "name": "response-iterator"}');
-    try {
-      toText(responseIterator(res)).then(function (data) {
-        assert.deepEqual(JSON.parse(data).name, 'response-iterator');
-        done();
-      });
-    } catch (err) {
-      done(err);
-    }
+describe('exports .mjs', () => {
+  it('exists', () => {
+    assert.equal(typeof responseIterator, 'function');
   });
 });
