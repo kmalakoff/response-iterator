6bea3d359b25ce235d8e8791f0ab1e0c505be878
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-14 09:09:38 -0700

    Comprehensive types

--- src/iterators/reader.ts
+++ src/iterators/reader.ts
@@ -1,19 +1,19 @@
 const hasIterator = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
 
 /* c8 ignore start */
-export default function readerIterator<T>(reader): AsyncIterableIterator<T> {
+export default function readerIterator<T, TReturn = unknown, TNext = unknown>(reader: ReadableStreamReader<T>): AsyncIterableIterator<T, TReturn, TNext> {
   const iterator = {
-    next(): Promise<IteratorResult<T, boolean>> {
-      return reader.read();
+    next(): Promise<IteratorResult<T, TReturn>> {
+      return reader.read(undefined) as Promise<IteratorResult<T, TReturn>>;
     },
   };
 
   if (hasIterator) {
-    iterator[Symbol.asyncIterator] = function (): AsyncIterator<T> {
+    iterator[Symbol.asyncIterator] = function (): AsyncIterator<T, TReturn> {
       return this;
     };
   }
 
-  return iterator as AsyncIterableIterator<T>;
+  return iterator as AsyncIterableIterator<T, TReturn, TNext>;
 }
 /* c8 ignore stop */
