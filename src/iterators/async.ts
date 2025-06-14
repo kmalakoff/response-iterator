806702e29cea3588304ca0cb8aa785caf7191018
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-14 09:09:38 -0700

    Comprehensive types

--- src/iterators/async.ts
+++ src/iterators/async.ts
@@ -1,11 +1,11 @@
-export default function asyncIterator<T>(source): AsyncIterableIterator<T> {
+export default function asyncIterator<T, TReturn = unknown, TNext = unknown>(source: AsyncIterableIterator<T, TReturn, TNext>): AsyncIterableIterator<T, TReturn, TNext> {
   const iterator = source[Symbol.asyncIterator]();
   return {
-    next(): Promise<IteratorResult<T, boolean>> {
+    next(): Promise<IteratorResult<T, TReturn>> {
       return iterator.next();
     },
-    [Symbol.asyncIterator](): AsyncIterator<T> {
+    [Symbol.asyncIterator](): AsyncIterator<T, TReturn, TNext> {
       return this;
     },
-  } as AsyncIterableIterator<T>;
+  } as AsyncIterableIterator<T, TReturn, TNext>;
 }
