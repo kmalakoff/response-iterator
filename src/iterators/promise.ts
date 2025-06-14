ec67adb65a4dcfbed572b58e9f68c23441b8f591
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-14 09:09:38 -0700

    Comprehensive types

--- src/iterators/promise.ts
+++ src/iterators/promise.ts
@@ -1,17 +1,17 @@
 const hasIterator = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
 
 /* c8 ignore start */
-export default function promiseIterator<T>(promise): AsyncIterableIterator<T> {
+export default function promiseIterator<T, TReturn = unknown, TNext = unknown>(promise: Promise<T>): AsyncIterableIterator<T, TReturn, TNext> {
   let resolved = false;
 
   const iterator = {
-    next(): Promise<IteratorResult<T, boolean>> {
+    next(): Promise<IteratorResult<T, TReturn>> {
       if (resolved) return Promise.resolve({ value: undefined, done: true });
       resolved = true;
       return new Promise((resolve, reject) => {
         promise
-          .then((value) => {
-            resolve({ value, done: false });
+          .then((value: T) => {
+            resolve({ value, done: false } as IteratorResult<T, TReturn>);
           })
           .catch(reject);
       });
@@ -19,11 +19,11 @@ export default function promiseIterator<T>(promise): AsyncIterableIterator<T> {
   };
 
   if (hasIterator) {
-    iterator[Symbol.asyncIterator] = function (): AsyncIterator<T> {
+    iterator[Symbol.asyncIterator] = function (): AsyncIterator<T, TReturn> {
       return this;
     };
   }
 
-  return iterator as AsyncIterableIterator<T>;
+  return iterator as AsyncIterableIterator<T, TReturn, TNext>;
 }
 /* c8 ignore stop */
