8adc2b80f9b52764070f52c6dc13211326920d99
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 09:12:29 -0700

    Upgrade modules

--- test/lib/stringStream.node.ts
+++ test/lib/stringStream.node.ts
@@ -1,5 +1,6 @@
-import Stream from 'stream';
 import StreamCompat from 'readable-stream';
+import Stream from 'stream';
+
 const Readable = Stream.Readable || StreamCompat.Readable;
 
 export default function stringStream(string: string, encoding: BufferEncoding): NodeJS.ReadableStream {
